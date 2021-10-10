cf8e4d749a5b4c8bd7a859cfe007cef69fa806b2
ChapJun <junc94@naver.com>
2021-10-10 19:26:34 +0900

    Initial commit

---
+++ first-service/src/test/java/com/chapjun/firstservice/FirstServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chapjun.firstservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
